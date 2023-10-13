ce308452cf37715ebdcc97fa26043204b42bee4b
nandobfer <nandobfer@yahoo.com.br>
2023-10-12 22:53:34 -0300

    update: get endoiint

--- routes.ts
+++ routes.ts
@@ -15,7 +15,21 @@ router.get("/", async (request: Request, response: Response) => {
     const premiados = canecas + produtos
     const total = canecas + produtos + nada
 
-    response.json({ total, premiados, nada, canecas, produtos })
+    const currentTime = new Date()
+
+    const currentHour = currentTime.getHours()
+    const eventStartTime = 11
+    const prizesPerHour = 20
+    const totalPrizesWonSoFar = await prisma.user.count({
+        where: {
+            NOT: [{ prize: null }, { prize: 0 }],
+        },
+    })
+    const totalPossiblePrizes = (currentHour - eventStartTime + 1) * prizesPerHour
+    const unclaimedPrizes = totalPossiblePrizes - totalPrizesWonSoFar
+    const availablePrizes = prizesPerHour + unclaimedPrizes
+
+    response.json({ total, premiados, nada, canecas, produtos, disponiveis_nessa_hora: availablePrizes })
 })
 
 router.use("/signup", signup)
