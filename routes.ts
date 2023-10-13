c8027b7981ecb2cc56df33acd3c6d3453cca7b3f
nandobfer <nandobfer@yahoo.com.br>
2023-10-12 22:49:33 -0300

    feat: added get endpoint

--- routes.ts
+++ routes.ts
@@ -1,8 +1,22 @@
 import express, { Express, Request, Response } from "express"
 import signup from "./src/signup"
 import roulette from "./src/roulette"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 export const router = express.Router()
+
+router.get("/", async (request: Request, response: Response) => {
+    const canecas = await prisma.user.count({ where: { prize: 1 } })
+    const produtos = await prisma.user.count({ where: { prize: 3 } })
+    const nada = await prisma.user.count({ where: { prize: 0 } })
+
+    const premiados = canecas + produtos
+    const total = canecas + produtos + nada
+
+    response.json({ total, premiados, nada, canecas, produtos })
+})
 
 router.use("/signup", signup)
 router.use("/roulette", roulette)
